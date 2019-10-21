48838506be78f6136a33153e7c20e6e0e504ed99
PetrHala <petr.hala@profinit.eu>
2019-10-21 14:17:22 +0200

    Commiting my work

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
 ## Put comments here that give an overall description of what your
-## functions do, this is just and edit
+## functions do
+
+## Function makeCacheMatrix takes an matrix as an argument and cre
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+	inverse <- NULL
+  	setmatrix <- function(a){       # sets new matrix into the object, and erases old cached inverse
+    		x <<- a
+    		inverse <<- NULL
+  	}
+  	matrix <- function(){           # returns the matrix stored in the mkmatrix object
+    		x
+  	}
+  	setinv <- function(i){          # sets externally computed inverse into the mkmatrix object
+    		inverse <<- i
+  	}
+  	inv <- function(){              # returns cached inverse or default NULL
+    		inverse
+  	}
+  	list(setmatrix = setmatrix, matrix = matrix, setinv = setinv, inv = inv)
 }
 
 
@@ -12,4 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$inv()                  	# stores cached inverse or null into inv
+        if(!is.null(inv)){
+                message("getting cached inverse")
+        } else {
+                inv <- solve(m$matrix())	# m$matrix() returns the matrix itself from the object created by mkmatrix
+                x$setinv(inv)           	# sets an inverse within the object created by mkmatrix
+        }
+        inv                             	# returns inv, either as stored cache or newly computed via the else statement  
 }
+
+
+
